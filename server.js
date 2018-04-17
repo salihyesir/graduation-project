3138759f6b5ccb888694049ae81163e9fa4ef6da
Salih Yesir <salih.yesir@ogr.sakarya.edu.tr>
2018-04-17 23:04:25 +0300

    social login
    passport-local, passport-google, passport-facebook, passport-twitter

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ var express = require('express'),
     path = require('path'),
     bodyParser = require('body-parser');
 
-
+var flash 		= require('connect-flash');
+//App components
+var routes 		= require('./app_server/routes');
+var session 	= require('./app_server/session');
+var passport    = require('./app_server/auth');
 
 
 
@@ -20,13 +24,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
+
+app.use('/', routes);
 var webServer = https.createServer(
     {
         key:  fs.readFileSync(path.join(__dirname, './private/privatekey.pem')),
         cert: fs.readFileSync(path.join(__dirname, './private/certificate.pem'))
     },
     app).listen(8443);
+
+
 
 // Middleware to catch 404 errors
 app.use(function(req, res, next) {
